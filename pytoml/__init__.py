7767aa37d6021ea673f4aa2212feaf72c4030283
bryant <bryant.wong@stonybrook.edu>
2013-02-25 01:37:34 +0100

    first release.

---
+++ pytoml/__init__.py
@@ -0,0 +1,80 @@
+from pyparsing import (
+    Combine, Optional, Regex, Forward, Group, Suppress, Keyword, LineEnd, Or,
+    ZeroOrMore, QuotedString, delimitedList, nums, Word, pythonStyleComment,
+    printables,
+)
+from datetime import datetime
+from re import sub
+
+class TOMLParser(object):
+    def __init__(self):
+        key_name = Word(sub(r"[\[\]=\"]", "", printables))
+        kgrp_name = Word(sub(r"[\[\]\.]", "", printables))
+        basic_int = Optional("-") + ("0" | Word(nums))
+
+        types = dict(
+            string = QuotedString("\""),
+            integer = Combine(basic_int),
+            float = Combine(basic_int + "." + Word(nums)),
+            datetime = Regex(r"\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z"),
+            boolean = Keyword("true") | Keyword("false"),
+            array = Forward(),
+        )
+
+        pure_array = Or(delimitedList(type_) for type_ in types.itervalues())
+        types["array"] << Group(Suppress("[") + Optional(pure_array) +
+                                Suppress("]"))
+
+        value = Or(type_ for type_ in types.itervalues())
+        keyvalue = key_name + Suppress("=") + value + Suppress(LineEnd())
+        keygroup_namespace = kgrp_name + ZeroOrMore(Suppress(".") + kgrp_name)
+        keygroup = "[" + keygroup_namespace + "]" + LineEnd()
+        comments = pythonStyleComment
+
+        self._toplevel = ZeroOrMore(keyvalue | keygroup)
+        self._toplevel.ignore(comments)
+
+        for k, v in types.iteritems():
+            v.setParseAction(getattr(self, "_parse_"+k))
+        keyvalue.setParseAction(self._parse_keyvalue)
+        keygroup_namespace.setParseAction(self._parse_keygroup_namespace)
+
+    _parse_string = lambda self, tok: tok[0].decode("unicode_escape")
+    _parse_integer = lambda self, tok: int(tok[0])
+    _parse_float = lambda self, tok: float(tok[0])
+    _parse_boolean = lambda self, tok: bool(tok[0])
+
+    ISO8601 = "%Y-%m-%dT%H:%M:%SZ"
+    _parse_datetime = lambda self, tok: datetime.strptime(tok[0], self.ISO8601)
+
+    _parse_array = lambda self, tok: [tok[0]]
+
+    def _parse_keyvalue(self, s, loc, toks):
+        k, v = toks.asList()
+        if k in self._cur:
+            raise NameError("key %s already exists" % k)
+        self._cur[k] = v
+
+    def _parse_keygroup_namespace(self, s, loc, toks):
+        cur = self._root
+        for subname in toks:
+            subspace = cur.get(subname, {})
+            if not isinstance(subspace, dict):
+                raise NameError("key %s already exists" % subname)
+            cur = cur.setdefault(subname, subspace)
+        self._cur = cur
+
+    def parse(self, s):
+        self._root = {}
+        self._cur = self._root
+        self._toplevel.parseWithTabs()
+        self._toplevel.parseString(s, parseAll=True)
+        return self._root
+
+def loads(string):
+    return TOMLParser().parse(string)
+
+if __name__ == "__main__":
+    from sys import stdin
+    from pprint import pprint
+    pprint(loads(stdin.read()))
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+install_requires = [
+    "pyparsing",
+]
+tests_require = [
+    "nose >= 1.0",
+]
+
+setup(
+    name = "pytoml",
+    version = "0.1",
+    description = "Python de-/serializer for TOML.",
+    url = "https://github.com/bryant/pytoml",
+    
+    license = "MIT License",
+    classifiers=[
+        'Programming Language :: Python',
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+
+    packages = find_packages(),
+    install_requires = install_requires,
+    tests_require = tests_require,
+)
